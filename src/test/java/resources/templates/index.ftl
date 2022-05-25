b4ec311381c7a0643c63ca8c0cdbbf461db14a90
Fleischerboy <78142977+Fleischerboy@users.noreply.github.com>
2022-05-25 04:03:33 +0200

    freemarker test

--- src/test/java/resources/templates/index.ftl
+++ src/test/java/resources/templates/index.ftl
@@ -4,6 +4,6 @@
     <meta charset="utf-8"/>
 </head>
 <body>
-<h1>Hello ${name}</h1>
+<h1>Hello ${firstName}</h1>
 </body>
 </html>
---
+++ src/test/java/test_freemarker_template_engine.java
@@ -0,0 +1,26 @@
+import model.User;
+import org.jphil.core.JPhil;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import static net.sourceforge.jwebunit.junit.JWebUnit.assertTextPresent;
+
+public class test_freemarker_template_engine {
+
+    @BeforeClass
+    public static void prepare() {
+        JPhil testApp = JPhil.startWebServer(7777);
+        testApp.get("/index", (request, response) -> {
+            response.renderTemplate("index", new User(1,"Philip", "Fleischer", "phila@hotmail.com", "phila", "phila123", "admin"));
+        });
+        setBaseUrl("http://localhost:7777");
+    }
+
+    @Test
+    public void freemarker() {
+        beginAt("/index");
+        assertTitleEquals("JPhil");
+        assertTextPresent("Hello Philip");
+    }
+}
